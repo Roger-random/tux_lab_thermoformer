78ab393b79d8b87c6214b2cc48319dea15d7a58e
Roger Cheng <roger.random@outlook.com>
2017-10-04 00:09:19 -0700

    Foiled by 'QMainWindow takes ownership of the widget pointer and deletes it at the appropriate time'

--- control_ui.py
+++ control_ui.py
@@ -1,4 +1,5 @@
 import sys
+from PyQt5.QtCore import QState, QStateMachine
 from PyQt5.QtWidgets import QApplication, QMainWindow, QHBoxLayout, QVBoxLayout, QPushButton, QLabel, QWidget
 
 # Name:Pin dictionary for the GPIO pins used as digital inputs.
@@ -52,12 +53,16 @@ statusIO = (
   ("Air", "Blow_Off_Valve")
 )
 
+# OnOffLabel class was designed to be used in the status bar to indicate
+# state of machine components. It has an "On" and an "Off" state that
+# is clearly visually distinct from each other.
+
 class OnOffLabel(QLabel):
   
   currentOn = False
   
-  def __init__(self, name, initialOn = False):
-    super().__init__(name)
+  def __init__(self, name, parent=None, initialOn = False):
+    super().__init__(name, parent)
     if(initialOn):
       self.turnOn()
     else:
@@ -71,13 +76,20 @@ class OnOffLabel(QLabel):
     self.currentOn = False
     self.setStyleSheet("QLabel {background-color : white; color : gray}")
     
+# Main Window class is the master of all UI on screen.
 
 class MainWindow(QMainWindow):
-  
+
+  # Initialization
   def __init__(self):
     super().__init__()
-    self.initUI()
-    
+    self.createStatusIO()
+    self.setGeometry(0, 0, 800, 480)
+    self.setWindowTitle("Tux Lab Thermoforming Control")
+    self.show() # Will need to be showFullScreen() on the Pi touchscreen.
+
+  # Walks through the statusIO list and create either an OnOffLabel
+  # (For those with an I/O label) or a normal QLabel (For those without.)
   def createStatusIO(self):
     self.ioLabels = dict()
     
@@ -85,21 +97,97 @@ class MainWindow(QMainWindow):
       if sio[1] == "":
         self.statusBar().addPermanentWidget(QLabel(sio[0]))
       else:
-        ioLabel = OnOffLabel(sio[0], sio[1] == "TableDownSW") # TableDownSW is a hack until I get real input
+        ioLabel = OnOffLabel(sio[0], initialOn = (sio[1] == "TableDownSW")) # TableDownSW is a hack until I get real input
         self.ioLabels[sio[1]] = ioLabel
         self.statusBar().addPermanentWidget(ioLabel)
     
-  def initUI(self):
-    self.createStatusIO()
-    self.setGeometry(0, 0, 800, 480)
-    self.setWindowTitle("Tux Lab Thermoforming Control")
-    self.show() # Will need to be showFullScreen() on the Pi touchscreen.
+  # Find a particular OnOffLabel with the given name
+  def findOnOffLabel(self, labelName):
+    if labelName in self.ioLabels:
+      return self.ioLabels[labelName]    
+
+# StandByState is the initial start-up state
+class StandbyState(QState):
+  def __init__(self, uiWindow, parent=None):
+    super().__init__(parent)
+    self.uiWindow = uiWindow
+    self.setupUI()
+    
+  # Set up the UI to be shown for this state
+  def setupUI(self):
+    self.ui = QWidget()
+    label = QLabel("Standby")
+    self.btnDC = QPushButton("Direct Control", self.ui)
+    
+    vbox = QVBoxLayout()
+    vbox.addWidget(label)
+    vbox.addWidget(self.btnDC)
+    self.ui.setLayout(vbox)
+
+  def onEntry(self, event):
+    print("StandbyState entered")
+    print(event)
+    self.uiWindow.setCentralWidget(self.ui)
+
+  # Called by state machine to connect transitions
+  def toDirectControl(self, directControl):
+    self.addTransition(self.btnDC.clicked, directControl)
+    
+
+# DirectControlState allows direct access to outputs
+class DirectControlState(QState):
+  def __init__(self, uiWindow, parent=None):
+    super().__init__(parent) 
+    self.uiWindow = uiWindow
+    self.setupUI()
+
+  # Set up the UI to be shown for this state
+  def setupUI(self):
+    self.ui = QWidget()
+    label = QLabel("Direct Control")
+    self.btnSB = QPushButton("Standby", self.ui)
+    
+    vbox = QVBoxLayout()
+    vbox.addWidget(label)
+    vbox.addWidget(self.btnSB)
+    self.ui.setLayout(vbox)
+    
+  def onEntry(self, event):
+    print("DirectControl entered")
+    print(event)
+    self.uiWindow.setCentralWidget(self.ui)
+    
+  def toStandby(self, standby):
+    self.addTransition(self.btnSB.clicked, standby)
+
+# Main state machine of the thermoformer control
+class StateMachine(QStateMachine):
+  def __init__(self, window, parent=None):
+    super().__init__(parent)
+    self.mainWindow = window
+    
+    standby = StandbyState(window)    
+    
+    directControl = DirectControlState(window)
+    
+    standby.toDirectControl(directControl)
+    directControl.toStandby(standby)
+
+    self.addState(directControl)
+    self.addState(standby)
+    
+    self.setInitialState(standby)
+
+# main function
 
 def main():
   global app
   app = QApplication(sys.argv)
   
-  mw = MainWindow()
+  mainWindow = MainWindow()
+  
+  stateMachine = StateMachine(window=mainWindow)
+  stateMachine.start()
   
   sys.exit(app.exec_())
 
